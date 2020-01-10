7df0e20e3f88dabafc360274007eae19343fb22f
michaeldelali <43325182+michaeldelali@users.noreply.github.com>
2020-01-10 10:07:00 +0700

    build: impact report

---
+++ internal/analyze/analyze_test.go
@@ -0,0 +1,14 @@
+package analyze
+
+import (
+	"testing"
+
+	"github.com/michaeldelali/contractfault/internal/contract"
+)
+
+// build constructs a minimal contract from endpoints and types for tests.
+func build(version string, types map[string]contract.Type, eps ...contract.Endpoint) *contract.Contract {
+	c := &contract.Contract{Service: "svc", Version: version, Types: types, Endpoints: eps}
+	return c
+}
+
