1d26d4a448071147e37e35a174805c3bccb4d627
michaeldelali <43325182+michaeldelali@users.noreply.github.com>
2020-02-20 18:43:00 +0700

    chore: bump pipeline gate

---
+++ internal/consumer/consumer.go
@@ -0,0 +1,15 @@
+// Package consumer models the usage manifests that named downstream services
+// publish to declare which parts of a contract they actually depend on.
+//
+// The impact analyzer joins the set of detected contract changes against these
+// manifests so that a report can say "change X breaks consumer Y" rather than
+// merely "change X exists". A manifest is deliberately coarse: it lists the
+// endpoints a consumer calls and the fields it reads or writes. That is enough
+// to compute a precise blast radius without forcing consumers to publish their
+// entire source tree.
+package consumer
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
