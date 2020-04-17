177a5c30e55370b5b9d51fb3bfd06e0b7837a3d4
michaeldelali <43325182+michaeldelali@users.noreply.github.com>
2020-04-17 13:03:00 +0700

    feat: add json schema

--- internal/consumer/consumer_test.go
+++ internal/consumer/consumer_test.go
@@ -3,3 +3,5 @@ package consumer
 import "testing"
 
 func TestCriticalityWeight(t *testing.T) {
+	cases := map[string]int{"high": 3, "medium": 2, "low": 1, "": 1, "bogus": 1}
+	for label, want := range cases {
