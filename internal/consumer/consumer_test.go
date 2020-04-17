868e2f7b24182cbe40c8d6af9a35a34efe1084e6
michaeldelali <43325182+michaeldelali@users.noreply.github.com>
2020-04-17 13:06:00 +0700

    feat: consumer scoring

--- internal/consumer/consumer_test.go
+++ internal/consumer/consumer_test.go
@@ -5,3 +5,6 @@ import "testing"
 func TestCriticalityWeight(t *testing.T) {
 	cases := map[string]int{"high": 3, "medium": 2, "low": 1, "": 1, "bogus": 1}
 	for label, want := range cases {
+		m := Manifest{Criticality: label}
+		if got := m.CriticalityWeight(); got != want {
+			t.Errorf("weight(%q) = %d, want %d", label, got, want)
