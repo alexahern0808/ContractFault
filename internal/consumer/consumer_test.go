044bee6cf8de8c74bde493ccbaa5e970b9920db5
michaeldelali <43325182+michaeldelali@users.noreply.github.com>
2020-05-28 14:45:00 +0700

    feat: pipeline gate

--- internal/consumer/consumer_test.go
+++ internal/consumer/consumer_test.go
@@ -29,3 +29,6 @@ func TestValidateDefaultsEmptyCriticality(t *testing.T) {
 	}
 }
 
+func TestFieldPathsUnion(t *testing.T) {
+	m := Manifest{Name: "c", Uses: []Usage{
+		{Endpoint: "a", ReadsFields: []string{"T.x"}, WritesFields: []string{"T.y"}},
