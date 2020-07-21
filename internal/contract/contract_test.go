8f32c8d37ee0a8f07a123b40cd7f70def4120c9d
michaeldelali <43325182+michaeldelali@users.noreply.github.com>
2020-07-21 11:03:00 +0700

    build: query builder

--- internal/contract/contract_test.go
+++ internal/contract/contract_test.go
@@ -10,3 +10,8 @@ const minimal = `{
   "version": "1.0.0",
   "types": {
     "Widget": { "kind": "object", "fields": { "id": { "type": "string", "required": true } } }
+  },
+  "endpoints": [
+    { "id": "getWidget", "method": "get", "path": "/w/{id}", "responses": { "200": "Widget" } }
+  ]
+}`
