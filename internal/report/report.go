e9cd79e2592562aa7eb6d21829b49bcf8937b12c
michaeldelali <43325182+michaeldelali@users.noreply.github.com>
2020-12-11 17:00:00 +0700

    test: cover report rendering

---
+++ internal/report/report.go
@@ -0,0 +1,11 @@
+// Package report renders an impact.Report into the two output formats
+// contractfault supports: deterministic pretty JSON for machines and the
+// TypeScript viewer, and a colorless, aligned text seismograph for humans and
+// CI logs. Both renderers are pure functions of the report so output is
+// reproducible byte-for-byte across runs.
+package report
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
