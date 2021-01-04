7ae7512fcde0d96d16fd06fd99ffc4ed4d05096b
michaeldelali <43325182+michaeldelali@users.noreply.github.com>
2021-01-04 16:23:00 +0700

    docs: document consumer graph

--- internal/report/report_test.go
+++ internal/report/report_test.go
@@ -8,3 +8,7 @@ import (
 	"github.com/michaeldelali/contractfault/internal/impact"
 )
 
+func TestExitCodeRupture(t *testing.T) {
+	r := &impact.Report{Summary: impact.Summary{Breaking: 1}}
+	if got := ExitCode(r, false); got != 2 {
+		t.Fatalf("breaking should exit 2, got %d", got)
