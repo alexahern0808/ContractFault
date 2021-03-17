40c9aedebe1a3864fdc80f07f5caa896eab65f74
michaeldelali <43325182+michaeldelali@users.noreply.github.com>
2021-03-17 11:11:00 +0700

    style: clean up threshold config

--- cmd/contractfault/main_test.go
+++ cmd/contractfault/main_test.go
@@ -4,3 +4,9 @@ import (
 	"os"
 	"path/filepath"
 	"testing"
+)
+
+// TestResolveConsumerPathsGlob verifies glob expansion, comma lists and
+// literal-path fallback all resolve deterministically.
+func TestResolveConsumerPathsGlob(t *testing.T) {
+	dir := t.TempDir()
