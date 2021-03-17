3a77c57fa8206661a21c170cf95b7c3233e396dc
michaeldelali <43325182+michaeldelali@users.noreply.github.com>
2021-03-17 11:19:00 +0700

    refactor: contract diff engine

--- cmd/contractfault/main_test.go
+++ cmd/contractfault/main_test.go
@@ -10,3 +10,8 @@ import (
 // literal-path fallback all resolve deterministically.
 func TestResolveConsumerPathsGlob(t *testing.T) {
 	dir := t.TempDir()
+	for _, n := range []string{"b.json", "a.json"} {
+		if err := os.WriteFile(filepath.Join(dir, n), []byte("{}"), 0o644); err != nil {
+			t.Fatal(err)
+		}
+	}
