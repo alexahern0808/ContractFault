828dcd7ac8daeacd1ba050244b7dac9c21a200a3
michaeldelali <43325182+michaeldelali@users.noreply.github.com>
2023-03-27 18:12:00 +0700

    feat: add viewer svg

---
+++ viewer/src/seismic.ts
@@ -0,0 +1,21 @@
+/**
+ * seismic renders a contractfault Report into two human-facing forms:
+ *
+ *  - renderTerminal: a colorized, aligned seismic map for the console, showing
+ *    the magnitude meter, per-change tremors and the consumer blast radius.
+ *  - renderSvg: a self-contained SVG "impact seismograph" that plots each
+ *    affected consumer as a station on a fault line, sized by its risk score.
+ *
+ * Both are pure functions of the Report so the viewer is deterministic.
+ */
+
+import type { Category, ConsumerImpact, Report, Severity } from "./types.js";
+
+const RESET = "\x1b[0m";
+const BOLD = "\x1b[1m";
+const RED = "\x1b[31m";
+const YELLOW = "\x1b[33m";
+const GREEN = "\x1b[32m";
+const CYAN = "\x1b[36m";
+const DIM = "\x1b[2m";
+
